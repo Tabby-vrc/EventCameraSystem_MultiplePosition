15ff6efa88291a5f50884f5b13e98c5a705ea740
Tabby <tabby1149@gmail.com>
2025-03-10 21:48:36 +0900

    ﻿:sparkles: オーナー自動取得、ディスプレイ撤去、Pickup撤去

---
+++ Script/OwnerController.cs
@@ -0,0 +1,13 @@
+﻿
+using UdonSharp;
+using VRC.SDKBase;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class OwnerController : UdonSharpBehaviour
+{
+    public void GetOwner()
+    {
+        if(!Networking.IsOwner(Networking.LocalPlayer, gameObject))
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+    }
+}
